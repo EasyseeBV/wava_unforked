9c68a20488e0ffe8da5df7fb441d85dcbd56fb0e
ahg95 <ansgar@easysee.nl>
2025-06-24 10:44:49 +0200

    Overhauled UI of captured screen.

--- Assets/Scripts/UI Support/Profile/ProfileUIManager.cs
+++ Assets/Scripts/UI Support/Profile/ProfileUIManager.cs
@@ -17,20 +17,6 @@ public class ProfileUIManager : MonoBehaviour
         Favorites
     }
 
-    [Header("Navigation Menus")]
-    [SerializeField] private Button photosButton;
-    [SerializeField] private TextMeshProUGUI photosLabel;
-    [SerializeField] private Button favoritesButton;
-    [SerializeField] private TextMeshProUGUI favoritesLabel;
-    [SerializeField] private Transform menuBar;
-    [SerializeField] private Color selectedColor;
-    [SerializeField] private Color unselectedColor;
-    [Space]
-    [SerializeField] private GameObject photosMenuObject;
-    [SerializeField] private GalleryItemsInstantiator galleryItemsInstantiator;
-    [SerializeField] private GameObject favoriteMenuObject;
-    [SerializeField] private FavoritesPage favoritesPage;
-
     [Header("Details")]
     public ItemDetailsUI galleryItemDetailsUI;
     
@@ -63,8 +49,6 @@ public class ProfileUIManager : MonoBehaviour
         }
 #endif
         
-        photosButton.onClick.AddListener(() => ChangeMenu(MenuNavigation.Photos));
-        favoritesButton.onClick.AddListener(() => ChangeMenu(MenuNavigation.Favorites));
         ChangeMenu(MenuNavigation.Photos);
     }
     
@@ -86,33 +70,6 @@ public class ProfileUIManager : MonoBehaviour
     private void ChangeMenu(MenuNavigation menu)
     {
         if (currentMenu == menu) return;
-
-        menuBar.transform.localPosition = new Vector3(
-            menu == MenuNavigation.Photos ? MENU_BAR_PHOTOS : MENU_BAR_LIKED,
-            menuBar.transform.localPosition.y,
-            menuBar.transform.localPosition.z);
-
-        photosLabel.color = menu == MenuNavigation.Photos ? selectedColor : unselectedColor;
-        favoritesLabel.color = menu == MenuNavigation.Favorites ? selectedColor : unselectedColor;
-
-        switch (menu)
-        {
-            case MenuNavigation.Photos:
-                favoritesPage.Close();
-                favoriteMenuObject.SetActive(false);
-                photosMenuObject.SetActive(true);
-                galleryItemsInstantiator.Open();
-                // TODO: filter photos here, if it was wanted in the future
-                break;
-            case MenuNavigation.Favorites:
-                photosMenuObject.SetActive(false);
-                favoriteMenuObject.SetActive(true);
-                favoritesPage.Open();
-                favoritesPage.Filter(filters);
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(menu), menu, null);
-        }
         
         currentMenu = menu;
     }
@@ -122,7 +79,6 @@ public class ProfileUIManager : MonoBehaviour
         if (filters.Contains(filter)) return;
         
         filters.Add(filter);
-        favoritesPage.Filter(filters);
     }
 
     public void RemoveFilter(ProfileFilter.Filter filter)
@@ -130,6 +86,5 @@ public class ProfileUIManager : MonoBehaviour
         if (!filters.Contains(filter)) return;
 
         filters.Remove(filter);
-        favoritesPage.Filter(filters);
     }
 }
